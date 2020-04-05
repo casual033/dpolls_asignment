6cc62a4ffaf909697b07955ac8e0685adeed051f
Aleksa Antelj <aleksa.antelj@ecd.rs>
2020-04-05 13:55:55 +0200

    Create spring-boot project. Closes #3.

---
+++ dpolls-app/src/main/java/com/finbit/dpollsapp/DpollsAppApplication.java
@@ -0,0 +1,13 @@
+package com.finbit.dpollsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DpollsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DpollsAppApplication.class, args);
+    }
+
+}
---
+++ dpolls-app/src/test/java/com/finbit/dpollsapp/DpollsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finbit.dpollsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DpollsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
